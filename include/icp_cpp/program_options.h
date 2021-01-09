2e309a62784dcf7c5cd5d638d8384c2a36aa56a2
Kyle M. Medeiros <kyle-medeiros@kylemail.net>
2021-01-08 22:59:08 -0800

    Add cxxopts to libraries

--- include/icp_cpp/program_options.h
+++ include/icp_cpp/program_options.h
@@ -5,20 +5,28 @@
 #include <string>
 
 // Third party
-#include <cxxopts.hpp>
+#include <cxxopts/cxxopts.hpp>
 
 constexpr int kMaxNumDefaultIterations = 100;
 
 struct ProgramOptions {
   std::string mode{"sequential"};
+
   std::string input_scan_a_topic{"scan"};
-  std::string initial_guess_topic{"transform_matrix"};
   std::string input_scan_b_topic{"scan_b"};
+
   std::string input_scan_a_type{"laserscan"};
   std::string input_scan_b_type{"laserscan"};
-  std::string output_transform_matrix_topic{"transform_matrix"};
+
+  std::string initial_guess_topic{"odom"};
+  std::string initial_guess_type{"odometry"};
+
+  std::string output_transform_topic{"transform"};
+  std::string output_transform_type{"matrix"};
+  std::string frame_id{"icp_out"};
+
   int max_num_iterations = kMaxNumDefaultIterations;
-  std::string frame_id{"laser"};
+
   float stepwise_time_interval{0.01};
   bool show_each_step{false};
   bool pub_scan_a{false};
@@ -38,20 +46,24 @@ ProgramOptions ParseArgs(int argc, char** argv) {
     cxxopts::value<std::string>(program_options.mode)->default_value("sequential"))
     ("a,in_a", "input topic to receive scan A on",
     cxxopts::value<std::string>(program_options.input_scan_a_topic)->default_value("scan"))
-    ("g,init_guess", "input topic to receive initial guess on",
-    cxxopts::value<std::string>(program_options.initial_guess_topic)->default_value("transform_matrix"))
     ("b,in_b", "input topic to receive scan B on",
     cxxopts::value<std::string>(program_options.input_scan_b_topic)->default_value("scan_b"))
+    ("g,init_guess", "topic to receive initial guess transform on",
+    cxxopts::value<std::string>(program_options.initial_guess_topic)->default_value("odom"))
+    ("g,in_transform_type", "message type of initial guess ['multiarray', 'odometry']",
+    cxxopts::value<std::string>(program_options.initial_guess_type)->default_value("odometry"))
     ("a_type", "message type of scan A, ['laserscan' 'pointcloud' or 'pointcloud2']",
     cxxopts::value<std::string>(program_options.input_scan_a_type)->default_value("laserscan"))
     ("b_type", "message type of scan B, ['laserscan' 'pointcloud' or 'pointcloud2']",
     cxxopts::value<std::string>(program_options.input_scan_b_type)->default_value("laserscan"))
-    ("transform_topic", "topic to publish transformation matrix on",
-    cxxopts::value<std::string>(program_options.output_transform_matrix_topic)->default_value("transform_matrix"))
+    ("transform_topic", "topic to publish transform on",
+    cxxopts::value<std::string>(program_options.output_transform_topic)->default_value("transform"))
+    ("transform_out_type", "message type of output transform",
+    cxxopts::value<std::string>(program_options.output_transform_type)->default_value("transform"))
     ("i,max_num_iterations", "max scan matcher iterations before exiting",
     cxxopts::value<int>(program_options.max_num_iterations)->default_value(std::to_string(kMaxNumDefaultIterations)))
     ("f,frame_id", "tf frame id",
-    cxxopts::value<std::string>(program_options.frame_id)->default_value("laser"))
+    cxxopts::value<std::string>(program_options.frame_id)->default_value("icp_out"))
     ("t,time_interval", "time interval for the show_each_step debug mode (will run one iteration and sleep for this amount)",
     cxxopts::value<float>(program_options.stepwise_time_interval)->default_value("0.01"))
     ("s,show_each_step", "Run in a debug mode where at every iteration of ICP, the current transformed scan is published.",
