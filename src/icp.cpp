6a22ba3f3404dd27db7bebf3e7769ffbde1ea377
Kyle M. Medeiros <kyle-medeiros@kylemail.net>
2021-01-15 01:22:58 -0800

    Fix heterogenous point operations

--- src/icp.cpp
+++ src/icp.cpp
@@ -33,34 +33,37 @@ void ICP::CalcClosestNeighborsKDTree(const Eigen::MatrixXd &scan_a) {
 
 void ICP::SetScanA(const Eigen::MatrixXd &scan_a) {
   input_scan_a_ = scan_a;
-  curr_aligned_scan_a_ = input_scan_a_;
   num_pts_a_ = scan_a.rows();
-
-  curr_aligned_scan_a_ = Eigen::MatrixXd::Ones(num_pts_a_, kDimension);
-  scan_a_hg_ = Eigen::MatrixXd::Ones(num_pts_a_, kDimension+1);
-
-  for (int pt_index = 0; pt_index < num_pts_a_; pt_index++) {
-    scan_a_hg_.row(pt_index).head<3>() = scan_a.row(pt_index);
-  }
+  curr_aligned_scan_a_ = Eigen::MatrixXd::Ones(kDimension, num_pts_a_);
+  scan_a_hg_ = Eigen::MatrixXd::Ones(kDimension+1, num_pts_a_);
 
   // Transform homogeneous pointcloud by initial guess
-  scan_a_hg_ = scan_a_hg_ * initial_guess_;
+  for (int pt_index = 0; pt_index < num_pts_a_; pt_index++) {
+    Eigen::Vector3d pt = scan_a.row(pt_index);
+    Eigen::Vector4d pt_hg = {pt[0], pt[1], pt[2], 1.0};
+    Eigen::Vector4d transformed_pt = initial_guess_ * pt_hg;
+
+    scan_a_hg_.col(pt_index) = transformed_pt;
+    curr_aligned_scan_a_.col(pt_index) = transformed_pt.head<3>();
+  }
 }
 
 void ICP::SetScanB(const Eigen::MatrixXd &scan_b) {
-  num_pts_b_ = scan_b.rows();
-  scan_b_hg_ = Eigen::MatrixXd::Ones(num_pts_b_, kDimension+1);
+  scan_b_ = scan_b;
+  num_pts_b_ = scan_b_.rows();
+  scan_b_hg_ = Eigen::MatrixXd::Ones(kDimension+1, num_pts_b_);
 
-  pc_b_index_ = std::make_unique<NearestNeighborKDTree>(kDimension, std::cref(scan_b), 10);
+  pc_b_index_ = std::make_unique<NearestNeighborKDTree>(kDimension, std::cref(scan_b_), 10);
   pc_b_index_->index->buildIndex();
 
   for (int pt_index = 0; pt_index < num_pts_b_; pt_index++) {
-    scan_b_hg_.row(pt_index).head<3>() = scan_b.row(pt_index);
+    scan_b_hg_.col(pt_index).head<3>() = scan_b_.row(pt_index);
   }
 }
 
 Eigen::Matrix4d ICP::ComputeFinalTransform() {
-  transform_matr_ = util::BestFitTransform(input_scan_a_, curr_aligned_scan_a_);
+
+  transform_matr_ = util::BestFitTransform(input_scan_a_, curr_aligned_scan_a_.transpose());
   return transform_matr_;
 }
 
@@ -85,20 +88,20 @@ double ICP::RunOneIteration() {
   indices_.clear();
   dists_.clear();
 
-  CalcClosestNeighborsKDTree(curr_aligned_scan_a_);
+  CalcClosestNeighborsKDTree(curr_aligned_scan_a_.transpose());
 
-  Eigen::MatrixXd closest_pts_in_b = Eigen::MatrixXd::Ones(num_pts_a_, kDimension);
+  Eigen::MatrixXd closest_pts_in_b = Eigen::MatrixXd::Ones(kDimension, num_pts_a_);
 
   for (int pt_index = 0; pt_index < num_pts_a_; pt_index++) {
-      closest_pts_in_b.row(pt_index) = scan_b_hg_.row(indices_[pt_index]).head<3>();
+      closest_pts_in_b.col(pt_index) = scan_b_hg_.col(indices_[pt_index]).head<3>();
   }
 
-  transform_matr_ = util::BestFitTransform(curr_aligned_scan_a_, closest_pts_in_b);
+  transform_matr_ = util::BestFitTransform(curr_aligned_scan_a_.transpose(), closest_pts_in_b.transpose());
 
-  scan_a_hg_ = scan_a_hg_ * transform_matr_;
+  scan_a_hg_ = transform_matr_ * scan_a_hg_;
 
   for (int pt_index = 0; pt_index < num_pts_a_; pt_index++) {
-    curr_aligned_scan_a_.row(pt_index) = scan_a_hg_.row(pt_index).head<3>();
+    curr_aligned_scan_a_.col(pt_index) = scan_a_hg_.col(pt_index).head<3>();
   }
 
   return std::accumulate(dists_.begin(), dists_.end(), 0.0) / dists_.size();
