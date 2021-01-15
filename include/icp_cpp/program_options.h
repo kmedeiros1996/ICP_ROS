1ebcbcf268f24926531c49cbeccc62b26314236e
Kyle M. Medeiros <kyle-medeiros@kylemail.net>
2021-01-14 23:13:41 -0800

    Add debug option to enable publishers/subscribers (true by default)

--- include/icp_cpp/program_options.h
+++ include/icp_cpp/program_options.h
@@ -30,11 +30,14 @@ struct ProgramOptions {
   std::string output_transform_topic{"transform"};
   std::string frame_id{"laser"};
 
-  // Debug Stepwise mode options
+
+  // Debug Options
   float stepwise_time_interval{0.01};
   bool show_each_step{false};
 
-  // Debug Working Scan Publishers
+  bool enable_publishing{true};
+  bool enable_subscribing{true};
+
   bool pub_scan_a{false};
   bool pub_trans_scan_a{false};
   bool pub_scan_b{false};
@@ -55,34 +58,51 @@ ProgramOptions ParseArgs(int argc, char** argv) {
   options.add_options()
     ("m,mode", "scan match mode [sequential, a_to_b]",
     cxxopts::value<std::string>(program_options.mode)->default_value("sequential"))
+    ("i,max_num_iterations", "max scan matcher iterations before exiting",
+    cxxopts::value<int>(program_options.max_num_iterations)->default_value(std::to_string(kMaxNumDefaultIterations)))
+
     ("a,in_a", "input topic to receive scan A on",
     cxxopts::value<std::string>(program_options.input_scan_a_topic)->default_value("scan"))
     ("b,in_b", "input topic to receive scan B on",
     cxxopts::value<std::string>(program_options.input_scan_b_topic)->default_value("scan_b"))
-    ("g,init_guess", "topic to receive initial guess transform on",
-    cxxopts::value<std::string>(program_options.initial_guess_topic)->default_value("odom"))
-    ("in_transform_type", "message type of initial guess ['multiarray', 'odometry']",
-    cxxopts::value<std::string>(program_options.initial_guess_type)->default_value("odometry"))
     ("a_type", "message type of scan A, ['laserscan' 'pointcloud' or 'pointcloud2']",
     cxxopts::value<std::string>(program_options.input_scan_a_type)->default_value("laserscan"))
     ("b_type", "message type of scan B, ['laserscan' 'pointcloud' or 'pointcloud2']",
     cxxopts::value<std::string>(program_options.input_scan_b_type)->default_value("laserscan"))
+
+
+    ("g,init_guess", "topic to receive initial guess transform on",
+    cxxopts::value<std::string>(program_options.initial_guess_topic)->default_value("odom"))
+    ("in_transform_type", "message type of initial guess ['multiarray', 'odometry']",
+    cxxopts::value<std::string>(program_options.initial_guess_type)->default_value("odometry"))
+
+
     ("transform_topic", "topic to publish transform on",
     cxxopts::value<std::string>(program_options.output_transform_topic)->default_value("transform"))
-    ("i,max_num_iterations", "max scan matcher iterations before exiting",
-    cxxopts::value<int>(program_options.max_num_iterations)->default_value(std::to_string(kMaxNumDefaultIterations)))
     ("f,frame_id", "tf frame id",
     cxxopts::value<std::string>(program_options.frame_id)->default_value("laser"))
+
+
     ("t,time_interval", "time interval for the show_each_step debug mode (will run one iteration and sleep for this amount)",
     cxxopts::value<float>(program_options.stepwise_time_interval)->default_value("0.01"))
     ("s,show_each_step", "Run in a debug mode where at every iteration of ICP, the current transformed scan is published.",
     cxxopts::value<bool>(program_options.show_each_step))
+
+
+    ("enable_publishing", "enable publishing of all messages (Disable only for unit testing or debug)",
+    cxxopts::value<bool>(program_options.enable_publishing)->default_value("true"))
+    ("enable_subscribing", "enable subscription to topics (Disable only for unit testing or debug)",
+    cxxopts::value<bool>(program_options.enable_subscribing)->default_value("true"))
+
+
     ("pub_scan_a", "enable publishing of scan A",
     cxxopts::value<bool>(program_options.pub_scan_a))
     ("pub_trans_scan_a", "enable publishing of transformed scan A",
     cxxopts::value<bool>(program_options.pub_trans_scan_a))
     ("pub_scan_b", "enable publishing of scan B",
     cxxopts::value<bool>(program_options.pub_scan_b))
+
+
     ("out_a_topic", "Output topic to publish scan A",
     cxxopts::value<std::string>(program_options.output_scan_a_topic)->default_value("scan_a_out"))
     ("stepwise_a_topic", "Output topic to publish scan A (during stepwise publishing mode)",
@@ -91,6 +111,8 @@ ProgramOptions ParseArgs(int argc, char** argv) {
     cxxopts::value<std::string>(program_options.output_trans_scan_a_topic)->default_value("scan_a_transformed_out"))
     ("out_b_topic", "Output topic to publish scan B",
     cxxopts::value<std::string>(program_options.output_scan_b_topic)->default_value("scan_b_out"))
+
+
     ("q,queue_size", "queue_size",
     cxxopts::value<int>(program_options.queue_size)->default_value(std::to_string(10)))
     ("h,help", "Print usage info");
